603cf0c615ed5b3c0e181e18eeb20f225ae9e84d
ia7ck <23146842+ia7ck@users.noreply.github.com>
2023-08-09 00:59:42 +0900

    実験用のコードを削除

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use std::collections::HashMap;
 use std::rc::Rc;
 use std::sync::Mutex;
 
-use rand::prelude::*;
 use serde::Deserialize;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
@@ -23,69 +22,6 @@ struct Rect {
 #[derive(Deserialize)]
 struct Cell {
     frame: Rect,
-}
-
-fn draw_triangle(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-) {
-    context.set_fill_style(&wasm_bindgen::JsValue::from_str(&format!(
-        "rgb({}, {}, {})",
-        color.0, color.1, color.2
-    )));
-
-    let [(top_x, top_y), (left_x, left_y), (right_x, right_y)] = points;
-    context.move_to(top_x, top_y);
-    context.begin_path();
-    context.line_to(left_x, left_y);
-    context.line_to(right_x, right_y);
-    context.line_to(top_x, top_y);
-    context.close_path();
-    context.stroke();
-    context.fill();
-}
-
-fn sierpinski(
-    context: &web_sys::CanvasRenderingContext2d,
-    points: [(f64, f64); 3],
-    color: (u8, u8, u8),
-    depth: u8,
-) {
-    draw_triangle(context, points, color);
-    if depth > 0 {
-        let midpoint = |(px, py), (qx, qy)| ((px + qx) / 2.0, (py + qy) / 2.0);
-
-        let mut rng = thread_rng();
-        let next_color = (
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-            rng.gen_range(0..255),
-        );
-
-        let [top, left, right] = points;
-        let left_middle = midpoint(top, left);
-        let right_middle = midpoint(top, right);
-        let bottom_middle = midpoint(left, right);
-        sierpinski(
-            context,
-            [top, left_middle, right_middle],
-            next_color,
-            depth - 1,
-        );
-        sierpinski(
-            context,
-            [left_middle, left, bottom_middle],
-            next_color,
-            depth - 1,
-        );
-        sierpinski(
-            context,
-            [right_middle, bottom_middle, right],
-            next_color,
-            depth - 1,
-        );
-    }
 }
 
 async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
@@ -114,33 +50,6 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap();
 
     wasm_bindgen_futures::spawn_local(async move {
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
-        let error_tx = Rc::clone(&success_tx);
-        let image = web_sys::HtmlImageElement::new().unwrap();
-        let callback = Closure::once(move || {
-            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                success_tx.send(Ok(()));
-            }
-        });
-        let error_callback = Closure::once(move |err| {
-            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
-                error_tx.send(Err(err));
-            }
-        });
-        image.set_onload(Some(callback.as_ref().unchecked_ref()));
-        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
-        image.set_src("Idle (1).png");
-        success_rx.await;
-        context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-
-        sierpinski(
-            &context,
-            [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
-            (0, 255, 0),
-            5,
-        );
-
         let json = fetch_json("rhb.json")
             .await
             .expect("Could not fetch rhb.json");
@@ -148,7 +57,6 @@ pub fn main_js() -> Result<(), JsValue> {
         let sheet: Sheet = serde_wasm_bindgen::from_value(json)
             .expect("Could not convert rhb.json into a Sheet structure");
 
-        // -- copy --
         let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
         let success_tx = Rc::new(Mutex::new(Some(success_tx)));
         let error_tx = Rc::clone(&success_tx);
@@ -167,7 +75,6 @@ pub fn main_js() -> Result<(), JsValue> {
         image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
         image.set_src("rhb.png");
         success_rx.await;
-        // -- copy --
 
         let sprite = sheet.frames.get("Run (1).png").expect("Cell not found");
         context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
