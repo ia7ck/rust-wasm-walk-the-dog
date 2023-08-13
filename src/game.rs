f1dc19a7ad91a2f88165be30d3a29635ff192963
ia7ck <23146842+ia7ck@users.noreply.github.com>
2023-08-13 12:08:50 +0900

    背景が無限に続くようにする

--- src/game.rs
+++ src/game.rs
@@ -25,7 +25,7 @@ pub enum WalkTheDog {
 
 pub struct Walk {
     boy: RedHatBoy,
-    background: Image,
+    backgrounds: [Image; 2],
     stone: Image,
     platform: Platform,
 }
@@ -325,9 +325,19 @@ impl Game for WalkTheDog {
                 );
                 let image = engine::load_image("rhb.png").await?;
                 let rhb = RedHatBoy::new(sheet, image);
+                let background_width = background.width() as i16;
                 Ok(Box::new(WalkTheDog::Loaded(Walk {
                     boy: rhb,
-                    background: Image::new(background, Point { x: 0, y: 0 }),
+                    backgrounds: [
+                        Image::new(background.clone(), Point { x: 0, y: 0 }),
+                        Image::new(
+                            background,
+                            Point {
+                                x: background_width,
+                                y: 0,
+                            },
+                        ),
+                    ],
                     stone: Image::new(stone, Point { x: 150, y: 546 }),
                     platform,
                 })))
@@ -351,7 +361,16 @@ impl Game for WalkTheDog {
 
             walk.platform.position.x += walk.velocity();
             walk.stone.move_horizontally(walk.velocity());
-            walk.background.move_horizontally(walk.velocity());
+            let velocity = walk.velocity();
+            let [first_background, second_background] = &mut walk.backgrounds;
+            first_background.move_horizontally(velocity);
+            second_background.move_horizontally(velocity);
+            if first_background.right() < 0 {
+                first_background.set_x(second_background.right());
+            }
+            if second_background.right() < 0 {
+                second_background.set_x(first_background.right());
+            }
 
             for bounding_box in &walk.platform.bounding_boxes() {
                 if walk.boy.bounding_box().intersects(bounding_box) {
@@ -382,7 +401,9 @@ impl Game for WalkTheDog {
             height: 600.0,
         });
         if let WalkTheDog::Loaded(walk) = self {
-            walk.background.draw(renderer);
+            for background in &walk.backgrounds {
+                background.draw(renderer);
+            }
             walk.boy.draw(renderer);
             walk.stone.draw(renderer);
             renderer.draw_rect(walk.stone.bounding_box());
