78feea4b524944d853bcceba4c3d81c62ef68d32
ia7ck <23146842+ia7ck@users.noreply.github.com>
2023-08-19 17:15:49 +0900

    パフォーマンスチェック用にフレームレートを表示する

--- src/engine.rs
+++ src/engine.rs
@@ -112,6 +112,15 @@ impl Renderer {
         );
         self.context.stroke();
     }
+
+    #[allow(unused)]
+    pub fn draw_text(&self, text: &str, location: &Point) -> Result<()> {
+        self.context.set_font("16pt serif");
+        self.context
+            .fill_text(text, location.x.into(), location.y.into())
+            .map_err(|err| anyhow!("Error filling text {:#?}", err))?;
+        Ok(())
+    }
 }
 
 #[async_trait(?Send)]
@@ -146,13 +155,23 @@ impl GameLoop {
         let mut keystate = KeyState::new();
         *g.borrow_mut() = Some(browser::create_raf_closure(move |perf: f64| {
             process_input(&mut keystate, &mut keyevent_receiver);
-            game_loop.accumulated_delta += (perf - game_loop.last_frame) as f32;
+
+            let frame_time = perf - game_loop.last_frame;
+            game_loop.accumulated_delta += frame_time as f32;
+
             while game_loop.accumulated_delta > FRAME_SIZE {
                 game.update(&keystate);
                 game_loop.accumulated_delta -= FRAME_SIZE;
             }
             game_loop.last_frame = perf;
             game.draw(&renderer);
+
+            if cfg!(debug_assertions) {
+                unsafe {
+                    draw_frame_rate(&renderer, frame_time);
+                }
+            }
+
             browser::request_animation_frame(f.borrow().as_ref().unwrap()).unwrap();
         }));
         browser::request_animation_frame(
@@ -426,5 +445,27 @@ mod tests {
         };
         // rect1 の上側, rect2 の下側が交差
         assert_eq!(rect1.intersects(&rect2), true);
+    }
+}
+
+unsafe fn draw_frame_rate(renderer: &Renderer, frame_time: f64) {
+    static mut FRAMES_COUNTED: i32 = 0;
+    static mut TOTAL_FRAME_TIME: f64 = 0.0; // msec
+    static mut FRAME_RATE: i32 = 0; // 頻繁に更新すると見にくくなるので 1000ms に一回更新する
+
+    FRAMES_COUNTED += 1;
+    TOTAL_FRAME_TIME += frame_time;
+
+    if TOTAL_FRAME_TIME > 1000.0 {
+        FRAME_RATE = FRAMES_COUNTED;
+        TOTAL_FRAME_TIME = 0.0;
+        FRAMES_COUNTED = 0;
+    }
+
+    if let Err(err) = renderer.draw_text(
+        &format!("Frame Rate {}", FRAME_RATE),
+        &Point { x: 400, y: 60 },
+    ) {
+        error!("Could not draw text {:#?}", err);
     }
 }
